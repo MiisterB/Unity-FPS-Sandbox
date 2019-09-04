468d1c640d76c1f33f60dc2077a43ae41c2393f6
MiisterB <40004939+MiisterB@users.noreply.github.com>
2019-09-04 14:20:28 +0200

    Add player detector and moving object

---
+++ FPS-Sandbox/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    [SerializeField] public List<Transform> waypoints;
+    [SerializeField] public float speed;
+    public int waypointIndex = 0;
+    public bool activateMovement = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("a"))
+        {
+            activateMovement = true;
+        }
+
+        if(activateMovement)
+        {
+            MoveObject();
+        }
+        
+    }
+
+    void MoveObject()
+    {
+        if(waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = speed * Time.deltaTime;
+
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if(transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+    }
+}
--- FPS-Sandbox/Assets/Scripts/RocketTurret.cs
+++ FPS-Sandbox/Assets/Scripts/RocketTurret.cs
@@ -5,13 +5,36 @@ using UnityEngine;
 public class RocketTurret : MonoBehaviour
 {
     public SphereCollider playerDetector;
-    public GameObject rotationPoint;
+    public Transform rotationPoint;
+    public Transform playerPosition;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerPosition != null)
+        {
+            RotateToPlayer();
+        }
+    }
+
+    void RotateToPlayer()
+    {
+
     }
     
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag.Equals("Player"))
+        {
+            playerPosition = collider.GetComponent<Transform>();
+        }
+    }
 
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag.Equals("Player"))
+        {
+            playerPosition = null;
+        }
+    }
 }
